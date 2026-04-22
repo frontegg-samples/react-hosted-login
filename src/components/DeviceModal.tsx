9d408f679ab2188d40404f9175dbb3974a56f6ba
PavelShulga <pavel.shulga@frontegg.com>
2026-04-22 11:42:54 +0300

    Created a separate css file for DeviceModal

--- src/components/DeviceModal.tsx
+++ src/components/DeviceModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import validateDeviceCode from "../utils/validateDeviceCode";
+import "../DeviceModal.css"
 
 interface VerifyDeviceModalProps {
   onClose: () => void;
